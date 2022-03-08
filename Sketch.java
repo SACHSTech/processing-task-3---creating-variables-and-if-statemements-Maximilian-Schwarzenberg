91d2975dfe797c27e3ff1009f8e0e6745698a207
Maximilian Schwarzenberg <78431850+Maximilian-Schwarzenberg@users.noreply.github.com>
2022-03-08 00:15:11 +0000

    Rewrote a majority of the program

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,19 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-  float randx = random(-250, 400);
-  float randy = random(-250, 400);
+  
+  float housex = random(0, 200);
+  float housey = random(50, 200);
 
+  float sunx = random(25,375);
+
+  int hour = hour();
+  int min = minute();
+  int sec = second();
   
   /*
   int randx = 100; // 100 for original
-  int randy = 50; // 50 for original
+  int randy = 200; // 50 for original
   */
   
   /**
@@ -26,10 +32,16 @@ public class Sketch extends PApplet {
   public void setup() {
     // Renders background later on
 
-    // Outputs randx and randy
+    // Outputs housex and housey
     System.out.println("");
-    System.out.println("randx: " + randx);
-    System.out.println("randy: " + randy);
+    System.out.println("housex: " + housex);
+    System.out.println("housey: " + housey);
+    System.out.println("");
+    System.out.println("hours: " + hour);
+    System.out.println("minutes: " + min);
+    System.out.println("seconds: " + sec);
+    System.out.println("");
+    System.out.println("sunx: " + sunx);
     System.out.println("");
   }
 
@@ -38,63 +50,50 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     // Sky colour
-    if (randy == 50) { // On ground
-      background(135, 206, 235); // Dark blue
+    if (hour >= 7 && hour <= 19) { // Day
+      background(135, 206, 235); // Blue
     }
-    else if (randy < 50 && randy > -250) { // In sky
-      background(0, 0, 204); // Blue
-    }
-    else if (randy > 50 && randy < 400) { // Under ground
-      background(210, 255, 173); // Light green
+    else if (hour >= 21 && hour <= 24 || hour >= 0 && hour <= 5) { // Night
+      background(0, 0, 139); // Dark blue
     }
     else {
-      background(132, 37, 147); // Purple
-    }
-    
-    // Rectangle for grass
-    if (randy == 50) { // On ground
-      fill(86, 125, 70); // Green
-      rect(0, 300, 400, 100);
-    }
-    else if (randy < 50 && randy > -250) { // In sky
-      // render nothing
-    }
-    else if (randy > 50 && randy < 400) { // Under ground
-      fill(160, 82, 45); // Brown
-      rect(0, 300, 400, 100);
-    }
-    else {
-      // renders nothing
+      background(255, 153, 51); // Orange
     }
 
     // Circle for sun
-    if (randx == 100) { // Middle
-      fill(255, 233, 0); //Yellow
-      ellipse(50, 50, 50, 50);
+    if (hour >= 7 && hour <= 19) { // Day
+      fill(255, 233, 0); // Yellow
+      ellipse(sunx, 100, 100, 100);
     }
-    else if (randx < 100 && randx > 0) { // Left
-      fill(255, 233, 0); //Yellow
-      ellipse(50, 50, (randx*50)/50, (randx*50)/50);
-    }
-    else if (randx > 100 && randx < 400) { // Right
-      fill(255, 233, 0); //Yellow
-      ellipse(50, 50, (randx/50)*50, (randx/50)*50);
+    else if (hour >= 21 && hour <= 24 || hour >= 0 && hour <= 5) { // Night
+      fill(255, 255, 255); // White
+      ellipse(sunx, 100, 100, 100);
     }
     else {
-      // renders nothing
+      fill(255, 215, 0); // Orange
+      ellipse(sunx, 100, 100, 100);
     }
+    
+    // Rectangle for grass
+    fill(86, 125, 70); // Green
+    rect(0, 300, 400, 100);
     
     // Base square for house
     fill(255, 248, 220); // White
-    rect(randx, randy+50, 200, 200);
+    rect(housex, housey+50, 200, 200);
 
     // Triangle for roof
     fill(170, 1, 20); // Red
-    triangle(randx, randy+50, randx+200, randy+50, randx+100, randy);
+    triangle(housex, housey+50, housex+200, housey+50, housex+100, housey);
 
     // Rectangle for door
     fill(196, 164, 132); // Light brown
-    rect(randx+100, randy+150, 50, 100);
+    rect(housex+100, housey+150, 50, 100);
+
+    // Clock (hour, minutes, seconds)
+    text(+hour, 1, 10);
+    text(+min, 21, 10);
+    text(+sec, 41, 10);
   }
   
 }
